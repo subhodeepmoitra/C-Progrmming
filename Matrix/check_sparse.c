d6ec1bffb7ec120ab5bcc258af22daf8e2783c4d
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2025-08-03 18:30:53 +0530

    sparse representation triplet commit

--- Matrix/check_sparse.c
+++ Matrix/check_sparse.c
@@ -28,6 +28,33 @@ void displayMatrix(int row, int column, int matrix[row][column]){
     }
 }
 
+void sparseMatrixRepresentation(int row, int column, int matrix[row][column]){
+    int non_zero=0, zero=0;
+    for(int i=0; i<row; i++){
+        for(int j=0;j <column; j++){
+            if(matrix[i][j] != 0){
+                non_zero++;
+            } else {
+                zero++;
+            }
+        }
+    }
+    int sparse_matrix[non_zero][3], k=0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<column; j++){
+            if(matrix[i][j] != 0){
+                sparse_matrix[k][0] = i;
+                sparse_matrix[k][1] = j;
+                sparse_matrix[k][2] = matrix[i][j];
+                k++;
+            }
+        }
+    }
+    printf("The sparse matrix representation is: \n");
+    displayMatrix(non_zero, 3, sparse_matrix);    
+
+}
+
 int main(){
     int row, column;
     printf("Enter the number of rows: ");
@@ -47,6 +74,7 @@ int main(){
     if(status == 1){
         printf("Sparse Confirmed: Needs Representation \n");
         // representation function to be called here
+        sparseMatrixRepresentation(row, column, matrix);
     } else {
         printf("Sparse not confirmed \n");
     }
