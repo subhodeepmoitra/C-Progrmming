ec962be7333d97bbdc76348437b17f96690d819a
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2025-08-19 10:49:45 +0530

    bubble sort in DMA commit

---
+++ Pointers/Bubble_Sort_using_DMA.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void display(int *arr, int size){
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int *arr, int size){
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+            
+        }
+        
+    }
+    
+}
+
+int main(){
+    int n;
+    int *arr;
+
+    printf("Enter the no of elements: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n*sizeof(int));
+
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed...");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element at index %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+    display(arr, n);
+    return 0;
+}
